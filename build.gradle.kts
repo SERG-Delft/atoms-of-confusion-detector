25c74d7081bd94788de9416f26ff94cac142957a
Jorge Romeu Huidobro <jorge.romeu.huidobro@gmail.com>
2021-07-12 10:39:16 +0200

    Add parsers generated by ANTLR

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ dependencies {
     testImplementation(kotlin("test"))
     detektPlugins("io.gitlab.arturbosch.detekt:detekt-formatting:1.17.1")
     implementation("com.github.ajalt.clikt:clikt:3.2.0")
+    implementation("org.antlr:antlr4-runtime:4.9.2")
 }
 
 tasks.test {
