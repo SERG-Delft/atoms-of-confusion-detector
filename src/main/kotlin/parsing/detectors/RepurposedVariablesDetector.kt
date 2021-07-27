3d808a05487644514aaf9c08cf59cd5ecc56f758
Pavlos Makridis <p.makridis@student.tudelft.nl>
2021-07-27 16:27:29 +0200

    Add tests for repurposed variables

--- src/main/kotlin/parsing/detectors/RepurposedVariablesDetector.kt
+++ src/main/kotlin/parsing/detectors/RepurposedVariablesDetector.kt
@@ -7,14 +7,18 @@ import parsing.AtomsVisitor
 @Visit(JavaParser.ExprAssignmentContext::class)
 class RepurposedVariablesDetector(visitor: AtomsVisitor, graph: ConfusionGraph) : Detector(visitor, graph) {
 
+//    val variablesInForLoops = mutableSetOf()
+
     override fun detect(ctx: JavaParser.ExprAssignmentContext) {
         val assignee = ctx.assignee.text
         val assigned = ctx.assigned.text
         // check if variable is re-assigned to an independent value
-        if (assigned.contains(assignee)) graph.addAppearancesOfAtom(
-            Atom.REPURPOSED_VARIABLES,
-            visitor.fileName,
-            mutableSetOf(ctx.start.line)
-        )
+        if (!assigned.contains(assignee)) {
+            graph.addAppearancesOfAtom(
+                    Atom.REPURPOSED_VARIABLES,
+                    visitor.fileName,
+                    mutableSetOf(ctx.start.line)
+            )
+        }
     }
 }
--- src/test/kotlin/parsing/detectors/RepurposedVariablesDetectorTest.kt
+++ src/test/kotlin/parsing/detectors/RepurposedVariablesDetectorTest.kt
@@ -1,6 +1,8 @@
 package parsing.detectors
 
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 internal class RepurposedVariablesDetectorTest : DetectorTest() {
 
@@ -8,4 +10,34 @@ internal class RepurposedVariablesDetectorTest : DetectorTest() {
     fun setup() {
         this.detector = RepurposedVariablesDetector(this.visitor, this.graph)
     }
+
+
+    @Test
+    fun testClass() {
+        val code = "class A { public void foo() {int a = 42; a = 43;} }"
+        val atoms = this.runVisitorFile(code)
+        assertAtom(atoms, "REPURPOSED_VARIABLES")
+    }
+
+    @Test
+    fun testNotRepurposed() {
+        val code = "class A { public void foo() {int a = 42; a = a + 43;} }"
+        val atoms = this.runVisitorFile(code)
+        assertEquals(0, atoms.size)
+    }
+
+    @Test
+    fun testNestedForLoop() {
+        val code = "class A { " +
+                "   public void foo() { " +
+                "       for(int i = 0; i < 10; i++) {" +
+                "           for(int j = 0; j < 10; i++) {" +
+                "           }" +
+                "       } " +
+                "   } " +
+                "}"
+        val atoms = this.runVisitorFile(code)
+        assertAtom(atoms, "REPURPOSED_VARIABLES")
+    }
+
 }
